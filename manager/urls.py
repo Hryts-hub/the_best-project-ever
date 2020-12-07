95171695baf79971f65c393d0920d8beab127983
Hryts-hub <bbkisik@gmail.com>
2020-12-07 14:10:49 +0300

    add new app and endpoint

---
+++ manager/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from manager.views import hello
+
+urlpatterns = [
+    path("hello/<int:digit>/", hello),
+    path('hello/<str:name>/', hello),
+    path('hello/', hello),
+    #path("", MyPage.as_view(), name="the-main-page"),
+]
