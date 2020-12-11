7669e4d642a59b3af2270daf4b1ca9da0d7622a4
Hryts-hub <bbkisik@gmail.com>
2020-12-11 21:57:30 +0300

    4 lesson

--- manager/urls.py
+++ manager/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
+from manager.views import hello, MyPage, AddLike, AddLike2Comment, AddRate2Book, BookDetail
 
-from manager.views import hello, MyPage, AddLike, AddLike2Comment
 
 urlpatterns = [
     path("hello/<int:digit>/", hello),
@@ -8,5 +8,7 @@ urlpatterns = [
     path('hello/', hello),
     path("add_like/<int:id>/", AddLike.as_view(), name="add-like"),
     path("add_like_to_comment/<int:id>/", AddLike2Comment.as_view(), name="add-like-to-comment"),
+    path("add_rate_to_book/<int:id>/<str:rate>/", AddRate2Book.as_view(), name="add-rate"),
+    path("book_view_detail/<int:id>/", BookDetail.as_view(), name="book-detail"),
     path("", MyPage.as_view(), name="the-main-page"),
 ]
--- manager/views.py
+++ manager/views.py
@@ -31,6 +31,7 @@ class MyPage(View):
         comments = Prefetch("comments", comment_query)
         books = Book.objects.prefetch_related("authors", comments)
         context["books"] = books
+        context["rate"] = "3"
         return render(request, "index.html", context)
 
 
@@ -47,3 +48,15 @@ class AddLike2Comment(View):
             LikeCommentUser.objects.create(user=request.user, comment_id=id)
         return redirect("the-main-page")
 
+
+class AddRate2Book(View):
+    def get(self, request, id, rate):
+        return redirect("the-main-page")
+
+
+class BookDetail(View):
+    def get(self, request, id):
+        comment_query = Comment.objects.annotate(count_like=Count("users_like")).select_related("author")
+        comments = Prefetch("comments", comment_query)
+        book = Book.objects.prefetch_related("authors", comments).get(id=id)
+        return render(request, "book_detail.html", {"book": book, "rate": 2})
