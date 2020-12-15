436b5d6db3454eedbb308ed885cd104ddfcc9ede
Hryts-hub <bbkisik@gmail.com>
2020-12-15 21:05:36 +0300

    + fix for book_detail

--- manager/views.py
+++ manager/views.py
@@ -18,23 +18,27 @@ class MyPage(View):
         context = {}
         comment_query = Comment.objects.select_related("author")
         comments = Prefetch("comments", comment_query)
-        context['books'] = Book.objects.prefetch_related("authors", comments)  #
+        context['books'] = Book.objects.prefetch_related("authors", comments)
         context['range'] = range(1, 6)
         return render(request, "index.html", context)
 
 
 class AddLike2Comment(View):
-    def get(self, request, id):
+    def get(self, request, id_book, id_comment, location=None):
         if request.user.is_authenticated:
-            LikeCommentUser.objects.create(user=request.user, comment_id=id)
-        return redirect("the-main-page")
+            LikeCommentUser.objects.create(user=request.user, comment_id=id_comment)
+        if location is None:
+            return redirect("the-main-page")
+        return redirect("book-detail", id=id_book)
 
 
 class AddRate2Book(View):  #
-    def get(self, request, id, rate):
+    def get(self, request, id, rate, location=None):
         if request.user.is_authenticated:
             RateBookUser.objects.create(user=request.user, book_id=id, rate=rate)
-        return redirect("the-main-page")
+        if location is None:
+            return redirect("the-main-page")
+        return redirect("book-detail", id=id)
 
 
 class BookDetail(View):
@@ -42,4 +46,4 @@ class BookDetail(View):
         comment_query = Comment.objects.annotate(count_like=Count("users_like")).select_related("author")
         comments = Prefetch("comments", comment_query)
         book = Book.objects.prefetch_related("authors", comments).get(id=id)
-        return render(request, "book_detail.html", {"book": book, "rate": 2})
+        return render(request, "book_detail.html", {"book": book, "range": range(1, 6)})
