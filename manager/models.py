03fe8d7e0700ff9c5a2d33a47003e6dd93a3dcf0
Hryts-hub <bbkisik@gmail.com>
2020-12-15 19:10:15 +0300

    + Rating with annotation (detail-). changes in code singed by "#"

--- manager/models.py
+++ manager/models.py
@@ -33,21 +33,16 @@ class LikeBookUser(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_book_table")
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="liked_user_table")
+    rate = models.PositiveIntegerField(default=5)  #
 
-    # def save(self, **kwargs):
-    #     try:
-    #         super().save(**kwargs)
-    #     except:
-    #         LikeBookUser.objects.get(user=self.user, book=self.book).delete()
     def save(self, **kwargs):
         try:
             super().save(**kwargs)
-        except:
-            LikeBookUser.objects.get(user=self.user, book=self.book).delete()
-            self.book.likes -= 1
-        else:
-            self.book.likes += 1
-        self.book.save()
+            self.book.likes += 1  #
+        except:  #
+            lbu = LikeBookUser.objects.get(user=self.user, book=self.book)
+            lbu.rate = self.rate
+            lbu.save()
 
 
 class Comment(models.Model):
@@ -73,11 +68,6 @@ class LikeCommentUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_comment_table")
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="liked_comment_table")
 
-    # def save(self, **kwargs):
-    #     try:
-    #         super().save(**kwargs)
-    #     except:
-    #         LikeCommentUser.objects.get(user=self.user, comment=self.comment).delete()
     def save(self, **kwargs):
         try:
             super().save(**kwargs)
--- manager/views.py
+++ manager/views.py
@@ -1,9 +1,10 @@
-from django.db.models import Count, Prefetch
+from django.db.models import Count, Prefetch, Avg
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
 
-from manager.models import Book, LikeBookUser, LikeCommentUser, Comment
+from manager.models import Book, LikeCommentUser, Comment
+from manager.models import LikeBookUser as RateBookUser
 
 
 def hello(request, name="filipp", digit=None):
@@ -12,34 +13,25 @@ def hello(request, name="filipp", digit=None):
     return HttpResponse(f"hello {name}")
 
 
-# class MyPage(View):
-#     def get(self, request):
-#         context = {}
-#         comment_query = Comment.objects.annotate(
-#             count_like=Count("users_like")).select_related("author")
-#         comments = Prefetch("comments", comment_query)
-#         books = Book.objects.prefetch_related("authors", comments)
-#         context["books"] = books.annotate(
-#             count_like=Count("users_like"),
-#             # count_comment=Count("comments")
-#         )
-#         return render(request, "index.html", context)
 class MyPage(View):
     def get(self, request):
         context = {}
         comment_query = Comment.objects.select_related("author")
         comments = Prefetch("comments", comment_query)
-        books = Book.objects.prefetch_related("authors", comments)
+        books = Book.objects.annotate(                #
+            avg_rate=Avg("liked_user_table__rate"),  #
+            count_users=Count("liked_user_table"))  #
+        books = books.prefetch_related("authors", comments)  #
         context["books"] = books
-        context["rate"] = "3"
+        context["range"] = range(1, 6)  #
         return render(request, "index.html", context)
 
 
-class AddLike(View):
-    def get(self, request, id):
-        if request.user.is_authenticated:
-            LikeBookUser.objects.create(user=request.user, book_id=id)
-        return redirect("the-main-page")
+# class AddLike(View):
+#     def get(self, request, id):
+#         if request.user.is_authenticated:
+#             LikeBookUser.objects.create(user=request.user, book_id=id)
+#         return redirect("the-main-page")
 
 
 class AddLike2Comment(View):
@@ -49,8 +41,10 @@ class AddLike2Comment(View):
         return redirect("the-main-page")
 
 
-class AddRate2Book(View):
+class AddRate2Book(View):  #
     def get(self, request, id, rate):
+        if request.user.is_authenticated:
+            RateBookUser.objects.create(user=request.user, book_id=id, rate=rate)
         return redirect("the-main-page")
 
 
