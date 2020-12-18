dbb64e37991540193e8a3fa5d966911c7e4a90d5
Hryts-hub <bbkisik@gmail.com>
2020-12-18 21:00:15 +0300

    ** Add SugBook and slug-fields in LikeBookUser, Comment

--- manager/models.py
+++ manager/models.py
@@ -3,6 +3,51 @@ from django.db import models
 from slugify import slugify
 
 
+class SlugBook(models.Model):  #
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
+
+    slug = models.SlugField(primary_key=True)
+    id = models.PositiveIntegerField(default=0, unique=True)
+
+    title = models.CharField(
+        max_length=50,
+        verbose_name="название",
+        help_text="ну это типо погоняло книги"
+    )
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    text = models.TextField()
+    authors = models.ManyToManyField(User, related_name="slug_books")
+    count_rated_users = models.PositiveIntegerField(
+        default=0)  # likes
+    count_all_stars = models.PositiveIntegerField(
+        default=0)
+    rate = models.DecimalField(
+        decimal_places=2,
+        max_digits=3,
+        default=0.0)
+    users_like = models.ManyToManyField(
+        User,
+        # null=True,  #
+        through="manager.LikeBookUser",
+        related_name="liked_slug_books"
+    )
+
+    def __str__(self):
+        return f"{self.title}-{self.slug}"
+
+    def save(self, **kwargs):  #
+        if self.slug is None:
+            self.slug = slugify(self.title)
+            self.id += 1
+        try:
+            super().save(**kwargs)
+        except:
+            self.slug += str(self.id)
+            super().save(**kwargs)
+
+
 class Book(models.Model):
     class Meta:
         verbose_name = "Книга"
@@ -19,23 +64,22 @@ class Book(models.Model):
     count_rated_users = models.PositiveIntegerField(
         default=0)  # likes
     count_all_stars = models.PositiveIntegerField(
-        default=0)  #
+        default=0)
     rate = models.DecimalField(
         decimal_places=2,
         max_digits=3,
-        default=0.0)  #
+        default=0.0)
     users_like = models.ManyToManyField(
         User,
         through="manager.LikeBookUser",
         related_name="liked_books"
     )
-    slug = models.SlugField(null=True, unique=True)  #
+    slug = models.SlugField(null=True, unique=True)
 
-    # чтобы в админке был не object, а название книги и ее id
     def __str__(self):
         return f"{self.title}-{self.id}"
 
-    def save(self, **kwargs):  #
+    def save(self, **kwargs):
         if self.id is None:
             self.slug = slugify(self.title)
         try:
@@ -52,6 +96,8 @@ class LikeBookUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_book_table")
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="liked_user_table")
     rate = models.PositiveIntegerField(default=5)
+
+    slug = models.ForeignKey(SlugBook, null=True, on_delete=models.CASCADE, related_name="slug_liked_user_table")
 
     def save(self, **kwargs):
         try:
@@ -72,7 +118,10 @@ class Comment(models.Model):
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="comments")
+
+    slug = models.ForeignKey(SlugBook, null=True, on_delete=models.CASCADE, related_name="slug_comments")  #
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
     likes = models.PositiveIntegerField(default=0)
     users_like = models.ManyToManyField(
         User,
